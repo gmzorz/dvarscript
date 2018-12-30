26b3d152c43183d53f44f74c138c4e4acd77b981
gmzorz <gijs.oosterling@hotmail.nl>
2018-12-30 13:33:39 +0100

    Update anim.bat

--- anim.bat
+++ anim.bat
@@ -8,15 +8,16 @@ set bindKey=F11
 set vString=anim
 set output=out.cfg
 
-	set dvar[0]=cg_fov
-	set start[0]=80
-	set end[0]=20
+	set dvar[0]=r_lightTweakSunDirection
+	set start[0]=30 240 0
+	set end[0]=-89 240 0
 	
-	set dvar[1]=r_lightTweakSunDirection
-	set start[1]=-89 180 0
-	set end[1]=-20 250 0
+	set dvar[1]=r_lightTweakSunLight
+	set start[1]=0
+	set end[1]=2
 
-set steps=50
+set steps=500
+	REM no more than 9999 steps
 set wait=1
 
 
@@ -40,11 +41,11 @@ set /a "count=0" & echo >> "!count!.clc"
 for /f "tokens=2 delims==" %%a in ('set dvar[') do (
 	for %%b in (*.clc) do (
 		for %%c in (!start[%%~nb]!) do ( 
-			if not %%c == 0 ( set /a sn=%%c*100 ) else ( set /a "sn=1" )
+			if not %%c == 0 ( set /a sn=%%c*10000 ) else ( set /a "sn=1" )
 			echo | set /p=!sn! >> start.txt
 		)
 		for %%c in (!end[%%~nb]!) do ( 
-			if not %%c == 0 ( set /a en=%%c*100 ) else ( set /a "en=1" )
+			if not %%c == 0 ( set /a en=%%c*10000 ) else ( set /a "en=1" )
 			echo | set /p=!en! >> end.txt
 		)
 		if exist start.txt (
@@ -101,13 +102,30 @@ for /l %%a in (1,1,!steps!) do (
 					del *.flt /s /q >nul
 					set /a "newValue=(%%d+!addTo_%%b_%%~nf!)" 2>nul
 					set valOut=!newValue: =!
-					set valOut_=!valOut:~0,-2!.!valOut:~-2!
+					set valOut_=!valOut:~0,-4!.!valOut:~-4!
+					if !newValue! lss 10000 ( 
+						if !newValue! gtr 999 ( set "valOut_=0.!valOut:~-5!" )
+					)
+					if !newValue! lss 1000 ( 
+						if !newValue! gtr 99 ( set "valOut_=0.0!valOut:~-4!" )
+					)
+					if !newValue! lss 100 ( 
+						if !newValue! gtr 9 ( set "valOut_=0.00!valOut:~-3!" )
+					)
+					if !newValue! lss 10 (
+						if !newValue! gtr 0 ( set "valOut_=0.000!valOut:~-2!" )
+						if !newValue! gtr -10000 ( set "valOut_=!valOut:~0,-4!0.!valOut:~-4!" )
+						if !newValue! gtr -1000 ( set "valOut_=!valOut:~0,-3!0.0!valOut:~-3!" )
+						if !newValue! gtr -100 ( set "valOut_=!valOut:~0,-2!0.00!valOut:~-2!" )
+						if !newValue! gtr -10 ( set "valOut_=!valOut:~0,-1!0.00!valOut:~-1!" )
+					)
 					echo | set /p=!valOut_! >> d.txt 
 					echo | set /p=!valOut! >> d2.txt 
 				)
 				set /a "floatVal+=1" & echo >> !floatVal!.flt
 			)
 			for /f "tokens=*" %%e in (d.txt) do set "start_[%%~nc]=%%e"
+			
 			for /f "tokens=*" %%e in (d2.txt) do set "start[%%~nc]=%%e"
 			if exist d.txt del d.txt /s /q >nul
 			if exist d2.txt del d2.txt /s /q >nul
